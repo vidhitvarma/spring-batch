f77db254703ce969fcfb058be88ca03ec83def49
Vidhit Varma <vidhitvarma300@gmail.com>
2023-07-17 12:32:47 +0530

    error commit

--- employeesDataPartitions/src/main/java/com/old/employeesdata/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.old.employeesdata.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.old.employeesdata.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
